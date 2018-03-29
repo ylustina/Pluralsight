4464a8806edd8ebcb5f9df57a598676bc97e8bd1
Yana Lustina <yana.lustina@gmail.com>
2018-03-29 16:11:04 -0500

    PEP8

--- 01_Python - Getting Started/PyStudentManager/app.py
+++ 01_Python - Getting Started/PyStudentManager/app.py
@@ -4,22 +4,22 @@ from student import Student
 
 students = []
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def students_page():
-    if request.method == 'POST':
-        new_student_id = request.form.get('student-id', '')
-        new_student_name = request.form.get('name', '')
-        new_student_last_name = request.form.get('last-name', '')
+    if request.method == "POST":
+        new_student_id = request.form.get("student-id", "")
+        new_student_name = request.form.get("name", "")
+        new_student_last_name = request.form.get("last-name", "")
 
         new_student = Student(name=new_student_name, student_id=new_student_id)
         students.append(new_student)
 
-        return redirect(url_for('students_page'))
+        return redirect(url_for("students_page"))
 
-    return render_template('index.html', students=students)
+    return render_template("index.html", students=students)
 
 
 if __name__ == "__main__":
