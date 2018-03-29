6b182c17cad4e63d4a1a02e02af39b154b37e54b
Yana Lustina <yana.lustina@gmail.com>
2018-03-29 13:54:55 -0500

    Initial commit

---
+++ 01_Python - Getting Started/PyStudentManager/student.py
@@ -0,0 +1,19 @@
+students = []
+
+class Student:
+
+    school_name = 'Springfield Elementary School'
+
+    def __init__(self, name, student_id=0):
+        self.name = name
+        self.student_id = student_id
+        students.append(self)
+
+    def __str__(self):
+        return 'Student: ' + self.name
+
+    def get_name_capitalize(self):
+        return self.name.capitalize()
+
+    def get_school_name(self):
+        return self.school_name
