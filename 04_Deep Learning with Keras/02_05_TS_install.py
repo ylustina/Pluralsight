942e9f5b884a6110d696ba8f7cd353716bea9010
Yana Lustina <yana.lustina@gmail.com>
2018-03-29 18:19:52 -0500

    Add os

--- 04_Deep Learning with Keras/02_05_TS_install.py
+++ 04_Deep Learning with Keras/02_05_TS_install.py
@@ -1,5 +1,7 @@
 # To verify that Tensorflow is working
 import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 # Print version
 print("Tensorflow version is: " + str(tf.__version__))
