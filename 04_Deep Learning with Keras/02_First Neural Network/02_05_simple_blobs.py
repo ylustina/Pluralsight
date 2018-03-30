ad490ea7d626d575e40d1a77ee59866cb5929f3f
Yana Lustina <yana.lustina@gmail.com>
2018-03-29 20:55:33 -0500

    Add plot_data function

--- 04_Deep Learning with Keras/02_First Neural Network/02_05_simple_blobs.py
+++ 04_Deep Learning with Keras/02_First Neural Network/02_05_simple_blobs.py
@@ -1,4 +1,23 @@
 
-
+import os
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+
+# Helper functions
+
+# Plotting the data on a figure
+def plot_data(pl, X, y):
+    # plot class where y==0
+    pl.plot(X[y==0, 0], X[y==0, 1], 'ob', alpha=0.5)
+    # plot class where y==1
+    pl.plot(X[y==1, 0], X[y==1, 1], 'xr', alpha=0.5)
+    pl.legend(['0', '1'])
+    return pl
+
+
+
 
